0a3ff5817c39fe969d05f67c99da2524720f27b7
BornOfTheMoon <mun4444@yandex.ru>
2021-10-27 22:10:16 +1000

    create activity's fragments

---
+++ app/src/main/java/com/example/first_homework/screens/registration/RegistrationScreenActivity.kt
@@ -0,0 +1,43 @@
+package com.example.first_homework.screens.registration
+
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.example.first_homework.R
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
+import com.example.first_homework.databinding.RegistrationScreenBinding
+
+class RegistrationScreenActivity: AppCompatActivity() {
+    private lateinit var binding: RegistrationScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = RegistrationScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.goBackButton.setOnClickListener {
+            finish()
+        }
+
+        setupSexDropDownMenu()
+
+        enableLinks()
+    }
+
+    private fun setupSexDropDownMenu() {
+        val sexes = listOf(
+            getString(R.string.sex_man),
+            getString(R.string.sex_woman),
+            getString(R.string.sex_other))
+
+        val inputLayout = binding.sexInputLayout
+
+        val adapter = ArrayAdapter(baseContext , R.layout.items_list, sexes)
+        (inputLayout.editText as? MaterialAutoCompleteTextView)?.setAdapter(adapter)
+    }
+
+    private fun enableLinks() {
+        binding.importantInfo.movementMethod = LinkMovementMethod.getInstance()
+    }
+}
